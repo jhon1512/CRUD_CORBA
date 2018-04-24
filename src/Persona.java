7bcd4e1d317f934f4204a36046035fcc1805f2e3
JUAN SEVASTIAN GAITAN <jsgaitan@corhuila.edu.co>
2018-04-23 20:09:14 -0500

    se crea la clase persona y se agrega la sentencia sql para insertar.

---
+++ src/Persona.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+import conexion.Conexion;
+import org.omg.CORBA.ORB;
+
+public class Persona extends PersonaApp.PersonaPOA {
+    private ORB orb;
+    
+    @Override
+    public boolean insertarPersona
+    
+    
+    
+}
---
+++ src/PersonaApp/PersonaPOA_1.java
@@ -0,0 +1,6 @@
+import java.sql.*;
+import 
+
+public class PersonaPOA {
+    
+}
